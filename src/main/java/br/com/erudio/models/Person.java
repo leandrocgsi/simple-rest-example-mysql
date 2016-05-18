4adc3e2085a77d1eb1bf7ba21efedad118262ac4
Leandro da Costa Gonçalves <leandrocgsi@gmail.com>
2016-05-18 14:00:11 -0300

    Adding entity Person

--- src/main/java/br/com/erudio/models/Person.java
+++ src/main/java/br/com/erudio/models/Person.java
@@ -2,18 +2,35 @@ package br.com.erudio.models;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import org.springframework.hateoas.ResourceSupport;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+@Entity
+@Table(name="person")
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Person extends ResourceSupport implements Serializable{
     
     private static final long serialVersionUID = 1L;
     
+    @Id
+    @GeneratedValue
+    @Column(name="idPerson", nullable=false)
     private Long idPerson;
+    
+    @Column(name="firstName", nullable=false)
     private String firstName;
+    
+    @Column(name="lastName", nullable=false)
     private String lastName;
+    
+    @Column(name="address", nullable=false)
     private String address;
     
     public Person() {}
@@ -49,4 +66,29 @@ public class Person extends ResourceSupport implements Serializable{
     public void setAddress(String address) {
         this.address = address;
     }
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + ((idPerson == null) ? 0 : idPerson.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Person other = (Person) obj;
+		if (idPerson == null) {
+			if (other.idPerson != null)
+				return false;
+		} else if (!idPerson.equals(other.idPerson))
+			return false;
+		return true;
+	}
 }
